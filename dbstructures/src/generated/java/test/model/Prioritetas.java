b0b05c8641c8ef893f4347015eb91e03ff346864
Vytenis Bivainis <monrealis@gmail.com>
2017-04-05 00:41:07 +0300

    Add generated Prioritetas

---
+++ dbstructures/src/generated/java/test/model/Prioritetas.java
@@ -0,0 +1,443 @@
+package test.model;
+
+public class Prioritetas {
+    private String id;
+
+    private String algorithmId;
+
+    private String sch;
+
+    private String schOrdering;
+
+    private String grouptype;
+
+    private String checkedPriority;
+
+    private String relEducation;
+
+    private String fromRegion;
+
+    private String theritory;
+
+    private String prioritySum;
+
+    private String lanId;
+
+    private String grDisturbance;
+
+    private String rownum;
+
+    private String globalid;
+
+    private String age;
+
+    private String educationId;
+
+    private String workhours;
+
+    private String regdate;
+
+    private String aprovedate;
+
+    private String flag;
+
+    private String rowdate;
+
+    private String checkdate;
+
+    private String valid;
+
+    private String regidd;
+
+    private String upid;
+
+    private String checkresult;
+
+    private String gLabel;
+
+    private String birthdate;
+
+    private String rdAprovedate;
+
+    private String rdRegdate;
+
+    private String reEducationId;
+
+    private String reWorkhours;
+
+    private String rdRegtype;
+
+    private String prioritySum2;
+
+    private String rsPrioSum;
+
+    private String rsPrioCount;
+
+    private String prioSum;
+
+    private String language;
+
+    private String givenumberbytoppriority;
+
+    private String givenumberforcitychild;
+
+    private String givenumberfortheritorychildwhitexeptions;
+
+    private String givenumberforlastyearregistration;
+
+    private String givenumberforpriority;
+
+    private String givenumberbecousbothparentsandchildlivesincity;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getAlgorithmId() {
+        return algorithmId;
+    }
+
+    public void setAlgorithmId(String algorithmId) {
+        this.algorithmId = algorithmId == null ? null : algorithmId.trim();
+    }
+
+    public String getSch() {
+        return sch;
+    }
+
+    public void setSch(String sch) {
+        this.sch = sch == null ? null : sch.trim();
+    }
+
+    public String getSchOrdering() {
+        return schOrdering;
+    }
+
+    public void setSchOrdering(String schOrdering) {
+        this.schOrdering = schOrdering == null ? null : schOrdering.trim();
+    }
+
+    public String getGrouptype() {
+        return grouptype;
+    }
+
+    public void setGrouptype(String grouptype) {
+        this.grouptype = grouptype == null ? null : grouptype.trim();
+    }
+
+    public String getCheckedPriority() {
+        return checkedPriority;
+    }
+
+    public void setCheckedPriority(String checkedPriority) {
+        this.checkedPriority = checkedPriority == null ? null : checkedPriority.trim();
+    }
+
+    public String getRelEducation() {
+        return relEducation;
+    }
+
+    public void setRelEducation(String relEducation) {
+        this.relEducation = relEducation == null ? null : relEducation.trim();
+    }
+
+    public String getFromRegion() {
+        return fromRegion;
+    }
+
+    public void setFromRegion(String fromRegion) {
+        this.fromRegion = fromRegion == null ? null : fromRegion.trim();
+    }
+
+    public String getTheritory() {
+        return theritory;
+    }
+
+    public void setTheritory(String theritory) {
+        this.theritory = theritory == null ? null : theritory.trim();
+    }
+
+    public String getPrioritySum() {
+        return prioritySum;
+    }
+
+    public void setPrioritySum(String prioritySum) {
+        this.prioritySum = prioritySum == null ? null : prioritySum.trim();
+    }
+
+    public String getLanId() {
+        return lanId;
+    }
+
+    public void setLanId(String lanId) {
+        this.lanId = lanId == null ? null : lanId.trim();
+    }
+
+    public String getGrDisturbance() {
+        return grDisturbance;
+    }
+
+    public void setGrDisturbance(String grDisturbance) {
+        this.grDisturbance = grDisturbance == null ? null : grDisturbance.trim();
+    }
+
+    public String getRownum() {
+        return rownum;
+    }
+
+    public void setRownum(String rownum) {
+        this.rownum = rownum == null ? null : rownum.trim();
+    }
+
+    public String getGlobalid() {
+        return globalid;
+    }
+
+    public void setGlobalid(String globalid) {
+        this.globalid = globalid == null ? null : globalid.trim();
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age == null ? null : age.trim();
+    }
+
+    public String getEducationId() {
+        return educationId;
+    }
+
+    public void setEducationId(String educationId) {
+        this.educationId = educationId == null ? null : educationId.trim();
+    }
+
+    public String getWorkhours() {
+        return workhours;
+    }
+
+    public void setWorkhours(String workhours) {
+        this.workhours = workhours == null ? null : workhours.trim();
+    }
+
+    public String getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(String regdate) {
+        this.regdate = regdate == null ? null : regdate.trim();
+    }
+
+    public String getAprovedate() {
+        return aprovedate;
+    }
+
+    public void setAprovedate(String aprovedate) {
+        this.aprovedate = aprovedate == null ? null : aprovedate.trim();
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag == null ? null : flag.trim();
+    }
+
+    public String getRowdate() {
+        return rowdate;
+    }
+
+    public void setRowdate(String rowdate) {
+        this.rowdate = rowdate == null ? null : rowdate.trim();
+    }
+
+    public String getCheckdate() {
+        return checkdate;
+    }
+
+    public void setCheckdate(String checkdate) {
+        this.checkdate = checkdate == null ? null : checkdate.trim();
+    }
+
+    public String getValid() {
+        return valid;
+    }
+
+    public void setValid(String valid) {
+        this.valid = valid == null ? null : valid.trim();
+    }
+
+    public String getRegidd() {
+        return regidd;
+    }
+
+    public void setRegidd(String regidd) {
+        this.regidd = regidd == null ? null : regidd.trim();
+    }
+
+    public String getUpid() {
+        return upid;
+    }
+
+    public void setUpid(String upid) {
+        this.upid = upid == null ? null : upid.trim();
+    }
+
+    public String getCheckresult() {
+        return checkresult;
+    }
+
+    public void setCheckresult(String checkresult) {
+        this.checkresult = checkresult == null ? null : checkresult.trim();
+    }
+
+    public String getgLabel() {
+        return gLabel;
+    }
+
+    public void setgLabel(String gLabel) {
+        this.gLabel = gLabel == null ? null : gLabel.trim();
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate == null ? null : birthdate.trim();
+    }
+
+    public String getRdAprovedate() {
+        return rdAprovedate;
+    }
+
+    public void setRdAprovedate(String rdAprovedate) {
+        this.rdAprovedate = rdAprovedate == null ? null : rdAprovedate.trim();
+    }
+
+    public String getRdRegdate() {
+        return rdRegdate;
+    }
+
+    public void setRdRegdate(String rdRegdate) {
+        this.rdRegdate = rdRegdate == null ? null : rdRegdate.trim();
+    }
+
+    public String getReEducationId() {
+        return reEducationId;
+    }
+
+    public void setReEducationId(String reEducationId) {
+        this.reEducationId = reEducationId == null ? null : reEducationId.trim();
+    }
+
+    public String getReWorkhours() {
+        return reWorkhours;
+    }
+
+    public void setReWorkhours(String reWorkhours) {
+        this.reWorkhours = reWorkhours == null ? null : reWorkhours.trim();
+    }
+
+    public String getRdRegtype() {
+        return rdRegtype;
+    }
+
+    public void setRdRegtype(String rdRegtype) {
+        this.rdRegtype = rdRegtype == null ? null : rdRegtype.trim();
+    }
+
+    public String getPrioritySum2() {
+        return prioritySum2;
+    }
+
+    public void setPrioritySum2(String prioritySum2) {
+        this.prioritySum2 = prioritySum2 == null ? null : prioritySum2.trim();
+    }
+
+    public String getRsPrioSum() {
+        return rsPrioSum;
+    }
+
+    public void setRsPrioSum(String rsPrioSum) {
+        this.rsPrioSum = rsPrioSum == null ? null : rsPrioSum.trim();
+    }
+
+    public String getRsPrioCount() {
+        return rsPrioCount;
+    }
+
+    public void setRsPrioCount(String rsPrioCount) {
+        this.rsPrioCount = rsPrioCount == null ? null : rsPrioCount.trim();
+    }
+
+    public String getPrioSum() {
+        return prioSum;
+    }
+
+    public void setPrioSum(String prioSum) {
+        this.prioSum = prioSum == null ? null : prioSum.trim();
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language == null ? null : language.trim();
+    }
+
+    public String getGivenumberbytoppriority() {
+        return givenumberbytoppriority;
+    }
+
+    public void setGivenumberbytoppriority(String givenumberbytoppriority) {
+        this.givenumberbytoppriority = givenumberbytoppriority == null ? null : givenumberbytoppriority.trim();
+    }
+
+    public String getGivenumberforcitychild() {
+        return givenumberforcitychild;
+    }
+
+    public void setGivenumberforcitychild(String givenumberforcitychild) {
+        this.givenumberforcitychild = givenumberforcitychild == null ? null : givenumberforcitychild.trim();
+    }
+
+    public String getGivenumberfortheritorychildwhitexeptions() {
+        return givenumberfortheritorychildwhitexeptions;
+    }
+
+    public void setGivenumberfortheritorychildwhitexeptions(String givenumberfortheritorychildwhitexeptions) {
+        this.givenumberfortheritorychildwhitexeptions = givenumberfortheritorychildwhitexeptions == null ? null : givenumberfortheritorychildwhitexeptions.trim();
+    }
+
+    public String getGivenumberforlastyearregistration() {
+        return givenumberforlastyearregistration;
+    }
+
+    public void setGivenumberforlastyearregistration(String givenumberforlastyearregistration) {
+        this.givenumberforlastyearregistration = givenumberforlastyearregistration == null ? null : givenumberforlastyearregistration.trim();
+    }
+
+    public String getGivenumberforpriority() {
+        return givenumberforpriority;
+    }
+
+    public void setGivenumberforpriority(String givenumberforpriority) {
+        this.givenumberforpriority = givenumberforpriority == null ? null : givenumberforpriority.trim();
+    }
+
+    public String getGivenumberbecousbothparentsandchildlivesincity() {
+        return givenumberbecousbothparentsandchildlivesincity;
+    }
+
+    public void setGivenumberbecousbothparentsandchildlivesincity(String givenumberbecousbothparentsandchildlivesincity) {
+        this.givenumberbecousbothparentsandchildlivesincity = givenumberbecousbothparentsandchildlivesincity == null ? null : givenumberbecousbothparentsandchildlivesincity.trim();
+    }
+}
